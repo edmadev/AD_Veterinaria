882596b4c98a43e59f591055bc5a3248b45430ec
edma2 <edma2@Asus_edma>
2024-12-08 17:10:43 +0100

    se añadieron paquetes pero sin ninguna funcionalidad de momento

--- src/main/java/com/mycompany/biblioteca/main/Ejecuta.java
+++ src/main/java/com/mycompany/biblioteca/main/Ejecuta.java
@@ -2,7 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.biblioteca.DAO.relacional;
+package com.mycompany.biblioteca.main;
+
+import com.mycompany.biblioteca.DAO.relacional.Conexion;
 
 /**
  *
