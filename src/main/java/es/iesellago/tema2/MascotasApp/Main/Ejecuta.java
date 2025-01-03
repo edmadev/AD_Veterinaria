f65f8372e23ff60aba56d91f62979e8a996180f4
edmadev <edma26dev@gmail.com>
2025-01-04 00:05:35 +0100

    Refactorizado nombre de Execute por Main

--- src/main/java/es/iesellago/tema2/MascotasApp/Main/Ejecuta.java
+++ src/main/java/es/iesellago/tema2/MascotasApp/Main/Ejecuta.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package es.iesellago.tema2.MascotasApp.Executer;
+package es.iesellago.tema2.MascotasApp.Main;
 
 import es.iesellago.tema2.MascotasApp.Controller.ConexionMySQL;
 import es.iesellago.tema2.MascotasApp.Controller.ConsultasDML;
